3c9835ea153dbf7d8c5b4d3242a83feb98316896
Josh Stern <josh.stern95@gmail.com>
2015-11-02 00:08:47 -0500

    Began developing a way of interfacing with the user database

--- Server.js
+++ Server.js
@@ -3,22 +3,60 @@
  */
 
 
+
+
+
+var sqlite = require('sqlite3').verbose();
 var express = require('express');
 var app = express();
 
-var DataBase;
+// required to support parsing of POST request bodies
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+// Serving static files in Public folder
+app.use(express.static("Public"));
 
 
-// Serving static files
-app.use(express.static(__dirname));
-
+var counter = 0;
 app.post('/users', function (req, res) {
+    // Get DB file
+    var db = new sqlite.Database("users.db");
+    //Check for
+    db.serialize(function() {
+        db.run("INSERT INTO Users VALUES (\'"+ req.body.username +"\', \'"+ req.body.password +"\', \'"+ req.body.nm +"\')", function(err) {
+            console.log("A user with this UserName already exists!");
+        });
+    });
+    db.close();
+    res.send("OK");
+});
 
+app.get('/users', function (req, res) {
+    // Get DB file
+    var db = new sqlite.Database("users.db");
+    db.serialize(function() {
+        //Make sure users table exists
+        db.each("SELECT * FROM Users", function(err, row) {
+            console.log(row);
+        });
+    });
+    db.close();
+    res.send("OK");
 });
 
 var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
 
+    var db = new sqlite.Database("users.db");
+    db.serialize(function() {
+        //Make sure users table exists
+        db.run("CREATE TABLE IF NOT EXISTS Users (UserName varchar(128) UNIQUE, Password varchar(128), RealName varchar(128))");
+    });
+    db.close();
+
     console.log('Example app listening at http://%s:%s', host, port);
 });
+
+
