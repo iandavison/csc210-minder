f765cb3f9a0108a267672eda3d1ae16576c5904a
Josh Stern <josh.stern95@gmail.com>
2015-11-09 14:01:43 -0500

    Added extra login/create account functionality

--- Server.js
+++ Server.js
@@ -16,33 +16,51 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 // Serving static files in Public folder
 app.use(express.static("Public"));
+app.use("/Public/libs", express.static(__dirname + "/Public/libs"));
 
 
-var counter = 0;
-app.post('/users', function (req, res) {
+app.post('/users/create', function (req, res) {
     // Get DB file
     var db = new sqlite.Database("users.db");
+
     //Check for
-    db.serialize(function() {
-        db.run("INSERT INTO Users VALUES (\'"+ req.body.username +"\', \'"+ req.body.password +"\', \'"+ req.body.nm +"\')", function(err) {
+    db.run("INSERT INTO Users VALUES (\'"+ req.body.username +"\', \'"+ req.body.password +"\', \'"+ req.body.nm +"\')", function(err) {
+        console.log(err);
+        if(err == null) {
+            res.send("OK");
+        }
+        else {
             console.log("A user with this UserName already exists!");
-        });
+            res.send("FAIL");
+        }
     });
     db.close();
-    res.send("OK");
 });
 
-app.get('/users', function (req, res) {
+app.post('/users/login', function(req, res) {
     // Get DB file
     var db = new sqlite.Database("users.db");
-    db.serialize(function() {
-        //Make sure users table exists
-        db.each("SELECT * FROM Users", function(err, row) {
-            console.log(row);
-        });
+
+    //Check for
+    db.all("SELECT * FROM Users WHERE UserName=\'"+ req.body.username +"\' AND Password=\'"+ req.body.password +"\'", function(err, rows) {
+        if(rows.length > 0) {
+            res.send("OK");
+        }
+        else {
+            res.send("FAIL");
+        }
     });
     db.close();
-    res.send("OK");
+});
+
+app.get('/users/', function (req, res) {
+    // Get DB file
+    var db = new sqlite.Database("users.db");
+    console.log("Database----------");
+    db.all("SELECT * FROM Users", function(err, rows) {
+        res.send(JSON.stringify(rows));
+    });
+    db.close();
 });
 
 var server = app.listen(3000, function () {
@@ -50,10 +68,8 @@ var server = app.listen(3000, function () {
     var port = server.address().port;
 
     var db = new sqlite.Database("users.db");
-    db.serialize(function() {
-        //Make sure users table exists
-        db.run("CREATE TABLE IF NOT EXISTS Users (UserName varchar(128) UNIQUE, Password varchar(128), RealName varchar(128))");
-    });
+    //Make sure users table exists
+    db.run("CREATE TABLE IF NOT EXISTS Users (UserName varchar(128) UNIQUE, Password varchar(128), RealName varchar(128))");
     db.close();
 
     console.log('Example app listening at http://%s:%s', host, port);
